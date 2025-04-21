e2e6e6d3b28d924a5b3f686f88594d6319e5c4aa
Rue-ig <133841378+Rue-ig@users.noreply.github.com>
2025-04-21 14:07:32 -0400

    sign in page update

--- public/firebaseConfig.js
+++ public/firebaseConfig.js
@@ -1,11 +1,12 @@
 const firebaseConfig = {
-    apiKey: "AIzaSyBnyTSJNB9SOsx1-RPySp7OOzEOK06rM0c",
-    authDomain: "info1601-booksapp.firebaseapp.com",
-    projectId: "info1601-booksapp",
-    storageBucket: "info1601-booksapp.firebasestorage.app",
-    messagingSenderId: "568459881258",
-    appId: "1:568459881258:web:a37d518df9686e77120c53"
-  };
+  apiKey: "AIzaSyBXmDkyRuDR5iFlknRNhAHy3SW68hLM58I",
+  authDomain: "island-friends-9e70c.firebaseapp.com",
+  projectId: "island-friends-9e70c",
+  storageBucket: "island-friends-9e70c.firebasestorage.app",
+  messagingSenderId: "274448630785",
+  appId: "1:274448630785:web:716e738843f057e603ac00",
+  measurementId: "G-E17DDZ0MYF"
+};
 
 
   export default firebaseConfig;
